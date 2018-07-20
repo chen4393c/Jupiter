95b202088ef1d6dd1b5377dac8ec2f9a7e14bade
chen4393c <chen4393c@gmail.com>
2018-07-20 17:48:30 -0500

    Add Item class to process event data

---
+++ src/entity/Item.java
@@ -0,0 +1,76 @@
+package entity;
+
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/*
+ * TicketMaster response is dirty. Sometimes we may need to compute
+ * and generate fields by programming.
+ * */
+
+public class Item {
+	private String itemId;
+	private String name;
+	private double rating;
+	private String address;
+	private Set<String> categories;
+	private String imageUrl;
+	private String url;
+	private double distance;
+	
+	// getters
+	public String getItemId() {
+		return itemId;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public Set<String> getCategories() {
+		return categories;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	
+	/*
+	 * This function is to convert an Item object into a
+	 * JSONObject instance because in our application, front-end
+	 * code cannot understand Java class, it can only understand
+	 * JSON.
+	 * */
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("item_id", itemId);
+			obj.put("name", name);
+			obj.put("rating", rating);
+			obj.put("address", address);
+			obj.put("categories", new JSONArray(categories));
+			obj.put("image_url", imageUrl);
+			obj.put("url", url);
+			obj.put("distance", distance);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+	
+	/*
+	 * 
+	 * */
+}
