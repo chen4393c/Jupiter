535f75362cc35f034aabe5b359883abcad2ee102
chen4393c <chen4393c@gmail.com>
2018-07-22 17:32:25 -0500

    Create db related package

---
+++ src/db/DBConnection.java
@@ -0,0 +1,79 @@
+package db;
+
+import java.util.List;
+import java.util.Set;
+
+import entity.Item;
+
+public interface DBConnection {
+	/**
+	 * Close the connection
+	 * */
+	public void close();
+	
+	/**
+	 * Insert the favorite items for a user.
+	 * 
+	 * @param userId
+	 * @param itemIds
+	 * */
+	public void setFavoriteItems(String userId, List<String> itemIds);
+	
+	/**
+	 * Delete the favorite items for a user.
+	 * 
+	 * @param userId
+	 * @param itemIds
+	 * */
+	public void unsetFavoriteItems(String userId, List<String> itemIds);
+	
+	/**
+	 * Get the favorite items id for a user.
+	 * 
+	 * @param userId
+	 * @return itemIds
+	 * */
+	public Set<String> getFavoriteItemIds(String userId);
+	
+	/**
+	 * Get categories based on item id
+	 * 
+	 * @param itemId
+	 * @return set of categories
+	 * */
+	public Set<String> getCategories(String itemId);
+	
+	/**
+	 * Search items near a geolocation and a term(optional).
+	 * 
+	 * @param lat
+	 * @param lon
+	 * @param term(Nullable)
+	 * @return list of items
+	 * */
+	public List<Item> searchItems(double lat, double lon, String term);
+	
+	/**
+	 * Save item into db.
+	 * 
+	 * @param item
+	 * */
+	public void saveItem(Item item);
+	
+	/**
+	 * Get full name of a user for user login.
+	 * 
+	 * @param userId
+	 * @return full name of the user
+	 * */
+	public String getFullname(String userId);
+	
+	/**
+	 * Check whether the user credential is correct for user login.
+	 * 
+	 * @param userId
+	 * @param password
+	 * @return boolean
+	 * */
+	public boolean verifyLogin(String userId, String password);
+}
---
+++ src/db/DBConnectionFactory.java
@@ -0,0 +1,21 @@
+package db;
+
+public class DBConnectionFactory {
+	// Create different db instances: MySQL or MongoDB
+	private static final String DEFAULT_DB = "mysql";
+	
+	public static DBConnection getConnection(String db) throws IllegalArgumentException {
+		switch (db) {
+			case "mysql":
+				return null; // new MySQLConnection()
+			case "mongodb":
+				return null; // new MongoDBConnection()
+			default:
+				throw new IllegalArgumentException("Invalid db: " + db);
+		}
+	}
+	
+	public static DBConnection getConnection() throws IllegalArgumentException {
+		return getConnection(DEFAULT_DB);
+	}
+}
