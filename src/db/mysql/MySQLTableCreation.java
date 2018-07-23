b03695d2efd6b3eedd00fd58e2d6c9101bd9521f
chen4393c <chen4393c@gmail.com>
2018-07-23 11:50:54 -0500

    Update MySQLTableCreation.java to log useful info

--- src/db/mysql/MySQLTableCreation.java
+++ src/db/mysql/MySQLTableCreation.java
@@ -10,7 +10,14 @@ import java.sql.Connection;
  * */
 
 public class MySQLTableCreation {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
+		// 0. Ensure the driver is registered.
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+		} catch (Exception e) {
+			System.out.println("Driver class was not found. " + e.getMessage());
+		}
+
 		/*
 		 * This is java.sql.Connection instead of 
 		 * com.mysql.jdbc.Connection
@@ -18,77 +25,79 @@ public class MySQLTableCreation {
 		Connection conn = null;
 		
 		// 1. Connect to MySQL.
+		System.out.println("Connecting to " + MySQLDBUtil.URL);
 		try {
-			System.out.println("Connecting to " + MySQLDBUtil.URL);
-			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
 			conn = DriverManager.getConnection(MySQLDBUtil.URL);
 		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println("Connecting to MySQL failed. " + e.getMessage());
 		}
 		
 		if (conn == null) {
 			return;
 		}
 		
-		// 2. Drop tables in case they exist.
-		Statement stmt = conn.createStatement();
-		String sql = "DROP TABLE IF EXISTS history";
-		stmt.executeUpdate(sql);
-		
-		sql = "DROP TABLE IF EXISTS categories";
-		stmt.executeUpdate(sql);
-		
-		sql = "DROP TABLE IF EXISTS items";
-		stmt.executeUpdate(sql);
-		
-		sql = "DROP TABLE IF EXISTS users";
-		stmt.executeUpdate(sql);
-		
-		// 3. Create tables
-		sql = "CREATE TABLE items ("
-				+ "item_id VARCHAR(255) NOT NULL, "
-				+ "name VARCHAR(255), "
-				+ "rating FLOAT, "
-				+ "address VARCHAR(255), "
-				+ "image_url VARCHAR(255), "
-				+ "url VARCHAR(255), "
-				+ "distance FLOAT, "
-				+ "PRIMARY KEY (item_id)"
-				+ ")";
-		stmt.executeUpdate(sql);
-		
-		sql = "CREATE TABLE categories ("
-				+ "item_id VARCHAR(255) NOT NULL, " 
-				+ "category VARCHAR(255) NOT NULL, "
-				+ "PRIMARY KEY (item_id, category), " 
-				+ "FOREIGN KEY (item_id) REFERENCES items(item_id)"
-				+ ")";
-		stmt.executeUpdate(sql);
-		
-		sql = "CREATE TABLE users ("
-				+ "user_id VARCHAR(255) NOT NULL, "
-				+ "password VARCHAR(255) NOT NULL, "
-				+ "first_name VARCHAR(255), "
-				+ "last_name VARCHAR(255), "
-				+ "PRIMARY KEY (user_id)"
-				+ ")";
-		stmt.executeUpdate(sql);
-		
-		sql = "CREATE TABLE history ("
-				+ "user_id VARCHAR(255) NOT NULL, "
-				+ "item_id VARCHAR(255) NOT NULL, "
-				+ "last_favor_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, "
-				+ "PRIMARY KEY (user_id, item_id), "
-				+ "FOREIGN KEY (user_id) REFERENCES users(user_id), "
-				+ "FOREIGN KEY (item_id) REFERENCES items(item_id) "
-				+ ")";
-		stmt.executeUpdate(sql);
-		
-		// 4. Insert a fake user
-		sql = "INSERT INTO users VALUES"
-				+ "(\"1111\", \"2222\", \"Chaoran\", \"Chen\")";
-		stmt.executeUpdate(sql);
-		
+		try {
+			// 2. Drop tables in case they exist.
+			Statement stmt = conn.createStatement();
+			String sql = "DROP TABLE IF EXISTS history";
+			stmt.executeUpdate(sql);
+
+			sql = "DROP TABLE IF EXISTS categories";
+			stmt.executeUpdate(sql);
+
+			sql = "DROP TABLE IF EXISTS items";
+			stmt.executeUpdate(sql);
+
+			sql = "DROP TABLE IF EXISTS users";
+			stmt.executeUpdate(sql);
+
+			// 3. Create tables
+			sql = "CREATE TABLE items ("
+					+ "item_id VARCHAR(255) NOT NULL, "
+					+ "name VARCHAR(255), "
+					+ "address VARCHAR(255), "
+					+ "image_url VARCHAR(255), "
+					+ "url VARCHAR(255), "
+					+ "distance FLOAT, "
+					+ "PRIMARY KEY (item_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE categories ("
+					+ "item_id VARCHAR(255) NOT NULL, "
+					+ "category VARCHAR(255) NOT NULL, "
+					+ "PRIMARY KEY (item_id, category), "
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE users ("
+					+ "user_id VARCHAR(255) NOT NULL, "
+					+ "password VARCHAR(255) NOT NULL, "
+					+ "first_name VARCHAR(255), "
+					+ "last_name VARCHAR(255), "
+					+ "PRIMARY KEY (user_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE history ("
+					+ "user_id VARCHAR(255) NOT NULL, "
+					+ "item_id VARCHAR(255) NOT NULL, "
+					+ "last_favor_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, "
+					+ "PRIMARY KEY (user_id, item_id), "
+					+ "FOREIGN KEY (user_id) REFERENCES users(user_id), "
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id) "
+					+ ")";
+			stmt.executeUpdate(sql);
+
+			// 4. Insert a fake user
+			sql = "INSERT INTO users VALUES"
+					+ "(\"1111\", \"2222\", \"Chaoran\", \"Chen\")";
+			stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		System.out.println("Import is done successfully");
 	}
 }
