2f3df2a21cc20a8e0aeb72d081c9181cb6e38c07
chen4393c <chen4393c@gmail.com>
2018-07-28 23:46:29 -0500

    Remove redundant class files and modify db init program

--- src/db/mysql/MySQLTableCreation.java
+++ src/db/mysql/MySQLTableCreation.java
@@ -92,7 +92,7 @@ public class MySQLTableCreation {
 
 			// 4. Insert a fake user
 			sql = "INSERT INTO users VALUES"
-					+ "(\"1111\", \"2222\", \"Chaoran\", \"Chen\")";
+					+ "(\"1111\", \"3229c1097c00d497a0fd282d586be050\", \"Chaoran\", \"Chen\")";
 			stmt.executeUpdate(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
